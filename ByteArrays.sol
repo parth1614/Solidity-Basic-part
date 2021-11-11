6d402fc9e622ad8aa5b4b02edc7a4b1643af4d99
Parth Arora <60436959+parth1614@users.noreply.github.com>
2021-11-11 22:33:33 +0530

    Create ByteArrays.sol

---
+++ ByteArrays.sol
@@ -0,0 +1,17 @@
+ //SPDX-License-Identifier: GPL-3.0
+ 
+ pragma solidity >=0.5.0 <0.9.0;
+ 
+ contract arrays{
+     
+     //Byte arrays are immutable in nature
+     //Byte arrays are static in nature
+     
+    bytes2 public b1; //array of size 2 bytes
+    bytes3 public b2; //array of size 3 bytes
+    
+    function setter() public{
+        b1 = 'ab'; //it'll store the ASCII value of a & b at respective indexes
+        b2 = 'abc'; //It'll store the ASCII values of a,b and c at repective indexes
+    }
+ }
